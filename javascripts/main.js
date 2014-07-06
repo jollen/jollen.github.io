651d51f2ef4ce3a7e884193d159bd449a5c89942
Jollen Chen <jollen@moko365.com>
2014-07-06 16:06:08 +0800

    close https://github.com/jollen/my-booklog/issues/2

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,29 @@
 	$.ajax({
 		dataType: 'json',
 		url: 'http://booklog.io/1/post',
-	    success: function(response) {
+	    success: function(response, jqXHR, textStatus) {
 			$('#postTemplate')
 				.tmpl(response.posts)
 				.appendTo('#content');
+	    },
+	    complete: function(jqXHR, textStatus) {
+			$('[id=subject]').each(function(index) {
+				var me = $(this);
+				
+				me.on('click', function() {
+					var id = me.data('id');
+
+					$.ajax({
+						dataType: 'json',
+						url: 'http://booklog.io/1/post/' + id,
+						success: function(response, jqXHR, textStatus) {
+							$('#content').html('<h2>' 
+								+ response.post.content 
+								+ '</h2>');
+						},
+					});
+				});
+			});
 	    }
 	});
 }) ($);
