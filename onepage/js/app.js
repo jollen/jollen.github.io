e5babee9aaf6f054c41349ff948e7d7f13925e12
jollen <jollen@jollen.org>
2014-08-05 03:12:13 +0000

    update

--- onepage/js/app.js
+++ onepage/js/app.js
@@ -36,6 +36,34 @@ app.PostItem = Backbone.Model.extend({
     }
 });
 
+app.Post = Backbone.Model.extend({
+    url: function() {
+        return 'http://booklog.io/1/post' + this.get('id');
+    },
+    id: '53e04830210fc45b67000035',
+    defaults: {
+        "success": true,
+        "errors": [],
+        "errfor": {},
+        "post": {
+            "_id": "53d4b7e28ceee0942f000957",
+            "__v": 0,
+            "userCreated": {
+                "id": "530223ee98b4052056000009",
+                "time": "2014-07-27T08:27:14.832Z",
+                "name": "jollenchen"
+            },
+            "wchars": 0,
+            "date": "2014-07-27T08:27:14.832Z",
+            "isActive": true,
+            "tags": [],
+            "html": "",
+            "content": "* Backbone Model\n * url\n * save, fetch\n* Underscore (Template)\n* Backbone View\n * initialize()\n * render()",
+            "subject": "學習重點"
+        }
+    }
+});
+
 
 /**
 * VIEWS
@@ -55,7 +83,23 @@ app.PostItemView = Backbone.View.extend({
     }
 });
 
+app.PostView = Backbone.View.extend({
+    el: '#post',
+    initialize: function() {
+		this.model = new app.PostItem();
+        this.template = _.template($('#tmpl-post').html());
+        this.model.bind('change', this.render, this);
+        
+        this.model.fetch(); 
+    },
+    render: function() {
+ 		var htmlCodes = this.template(this.model.attributes);
+        this.$el.html(htmlCodes);
+    }
+});
+
 
 $(document).ready(function(){
     app.postItemView = new app.PostItemView();
+    app.postView = new app.PostView();
 });
