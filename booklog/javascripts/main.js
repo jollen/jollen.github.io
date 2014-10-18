c1c69478f95838257b392187b737c2e9967bf712
jollen <jollen@jollen.org>
2014-10-18 02:42:09 +0000

    fix AJAX

--- booklog/javascripts/main.js
+++ booklog/javascripts/main.js
@@ -1,19 +1,13 @@
 (function($) {
 	$.ajax({
 		dataType: 'json',
-		url: 'http://localhost:3000/1/post',
+		url: 'https://www.mokoversity.com/1/post/tags/startup',
 	    success: function(response, jqXHR, textStatus) {
 			$('#postTemplate')
 				.tmpl(response.posts)
 				.appendTo('#content');
 	    },
 	    complete: function(jqXHR, textStatus) {
-			$('[id=subject]').each(function(index) {
-				var me = $(this);
-				
-				me.on('click', function() {
-					var id = me.data('id');
-
 					$.ajax({
 						dataType: 'json',
 						url: 'http://localhost:3000/1/post/' + id,
