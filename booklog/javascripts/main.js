48f4198c5b8acc6db52326cf9dcd805d653b1bed
jollen <jollen@jollen.org>
2014-10-18 02:49:59 +0000

    bug fix

--- booklog/javascripts/main.js
+++ booklog/javascripts/main.js
@@ -8,17 +8,15 @@
 				.appendTo('#content');
 	    },
 	    complete: function(jqXHR, textStatus) {
-					$.ajax({
-						dataType: 'json',
-						url: 'http://localhost:3000/1/post/' + id,
-						success: function(response, jqXHR, textStatus) {
-							$('#content').html('<h2>' 
-								+ response.post.content 
-								+ '</h2>');
-						},
-					});
-				});
-			});
+            $.ajax({
+                dataType: 'json',
+                url: 'http://localhost:3000/1/post/' + id,
+                success: function(response, jqXHR, textStatus) {
+                    $('#content').html('<h2>' 
+                        + response.post.content 
+                        + '</h2>');
+                },
+            });
 	    }
 	});
 }) ($);
