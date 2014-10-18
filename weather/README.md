5c7e57ee793509fe522a50e2e8ce96735c0daa0d
jollen <jollen@jollen.org>
2014-10-18 03:44:15 +0000

    use Weather Icons

---
+++ weather/README.md
@@ -0,0 +1,10 @@
+## 主要框架
+
+* 使用 Boostrap
+* 使用 jQuery
+* 使用 jQuery tmpl
+
+## 其它
+
+* Weather API, http://api.openweathermap.org/data/2.5/weather?q=Taipei
+* Weather Icons, http://erikflowers.github.io/weather-icons/
--- weather/javascripts/main.js
+++ weather/javascripts/main.js
@@ -4,12 +4,27 @@
 		url: 'http://api.openweathermap.org/data/2.5/weather?q=Taipei',
 	    success: function(response, jqXHR, textStatus) {
             var data = [];
+            var obj = {};
             
-            data.push(response.main);
+            obj.temp = response.main.temp;
+            obj.humidity = response.main.humidity;
+            
+            // Weather Icons
+            switch (response.weather[0].main) {
+                case "Clouds": obj.weatherIcon = "wi-day-cloudy";
+            }
+            
+            // Celsius
+            obj.celsius = parseInt(response.main.temp - 273.15);
+            
+            
+            data.push(obj);
             
 			$('#postTemplate')
 				.tmpl(data)
 				.appendTo('#content');
+            
+            $('#weather-icon').addClass('wi-day-cloudy');
 	    },
 	    complete: function(jqXHR, textStatus) {
 	    }
