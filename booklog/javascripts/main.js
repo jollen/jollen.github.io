b237ad9418eb68347a0a773a5b14ae147780de83
Jollen Chen <jollen@moko365.com>
2015-07-26 10:30:13 +0800

    update: booklog3 frontend

--- booklog/javascripts/main.js
+++ booklog/javascripts/main.js
@@ -1,22 +1,13 @@
 (function($) {
 	$.ajax({
 		dataType: 'json',
-		url: 'https://www.mokoversity.com/1/post/tags/startup',
+		url: 'http://localhost:3000/1/post',
 	    success: function(response, jqXHR, textStatus) {
 			$('#postTemplate')
 				.tmpl(response.posts)
 				.appendTo('#content');
 	    },
 	    complete: function(jqXHR, textStatus) {
-            $.ajax({
-                dataType: 'json',
-                url: 'http://localhost:3000/1/post/' + id,
-                success: function(response, jqXHR, textStatus) {
-                    $('#content').html('<h2>' 
-                        + response.post.content 
-                        + '</h2>');
-                },
-            });
 	    }
 	});
 }) ($);
