30016b0e47fe55fbbf0d3112a58f78e362983a21
jollen <jollen@moko365.com>
2016-01-09 14:23:22 +0800

    Update app.js

--- booklog/javascripts/app.js
+++ booklog/javascripts/app.js
@@ -9,7 +9,7 @@ var app = app || {};
 **/
 app.Message = Backbone.Model.extend({
     url: function() {
-         return 'http://launchergap.org/1/post' 
+         return 'http://localhost:3000/1/post' 
                     + ( this.id === null ? '' : '/' + this.id );
     },
     id: null,
